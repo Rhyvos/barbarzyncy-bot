a2c653b96dd3b5c5852507e62f560d3e2e0af41d
root <root@DESKTOP-B4040DA>
2024-03-17 00:09:56 +0100

    static fixes

--- BarbarzyncyBot/BotManager/settings.py
+++ BarbarzyncyBot/BotManager/settings.py
@@ -133,7 +133,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/barbarzyncy/discord/static/'
+STATIC_ROOT = '/home/fioyjnaspn/public_html/barbarzyncy/discord/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
