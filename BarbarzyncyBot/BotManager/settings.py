da7b434f38edba69dad406e3cc66c13d44fae29a
root <root@DESKTOP-B4040DA>
2024-03-25 22:20:02 +0100

    Updated Version, update script test

--- BarbarzyncyBot/BotManager/settings.py
+++ BarbarzyncyBot/BotManager/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
